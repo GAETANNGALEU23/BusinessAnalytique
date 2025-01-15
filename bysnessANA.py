2dd8692dedb0f98049d85311acff0512665cb584
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2025-01-15 15:21:01 +0100

    Update bysnessANA.py

--- bysnessANA.py
+++ bysnessANA.py
@@ -6,3 +6,10 @@ import streamlit as st
 
 # Titre de l'application
 st.title("Dashboard Interactif des Ventes")
+
+# Section Résumé 
+st.header("Vue d'ensemble")
+total_ventes = df['Montant'].sum() 
+total_transactions = df.shape[0] 
+montant_moyen = df['Montant'].mean() 
+satisfaction_moyenne = df['Satisfaction_Client'].mean()
