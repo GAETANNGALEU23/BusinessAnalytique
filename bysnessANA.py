74ca1e8bb92848ad74eededf516cfd5c29567dcd
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2025-01-15 15:14:59 +0100

    Update bysnessANA.py

--- bysnessANA.py
+++ bysnessANA.py
@@ -1,2 +1,8 @@
 import pandas as pd
 df = pd.read_csv('data_dashboard_large.csv')
+
+import streamlit as st 
+import plotly.express as px
+
+# Titre de l'application
+st.title("Dashboard Interactif des Ventes")
