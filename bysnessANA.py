939c8bf3b2b72988416f8ddffdddcdcd8c1cfbb3
GAETANNGALEU23 <francoisngaleu232@gmail.com>
2025-01-15 15:25:21 +0100

    Update bysnessANA.py

--- bysnessANA.py
+++ bysnessANA.py
@@ -2,7 +2,7 @@ import pandas as pd
 df = pd.read_csv('data_dashboard_large.csv')
 
 import streamlit as st 
-import plotly.express as px
+#import plotly.express as px
 
 # Titre de l'application
 st.title("Dashboard Interactif des Ventes")
@@ -13,3 +13,8 @@ total_ventes = df['Montant'].sum()
 total_transactions = df.shape[0] 
 montant_moyen = df['Montant'].mean() 
 satisfaction_moyenne = df['Satisfaction_Client'].mean()
+
+st.metric("Total des ventes (€)", f"{total_ventes:,.2f}")
+st.metric("Nombre total de transactions", total_transactions)
+st.metric("Montant moyen par transaction (€)", f"{montant_moyen:,.2f}")
+st.metric("Satisfaction client moyenne", f"{satisfaction_moyenne:.2f}")
